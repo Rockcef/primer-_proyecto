9401c7ec00428521467adf9504d919d9969c8a47
Pedro Corona <jorge.corona_86@hotmail.com>
2024-07-22 22:27:11 -0600

    Feat: unlinkSync

--- append-file.js
+++ append-file.js
@@ -3,5 +3,7 @@ const fs = require("node:fs")
 /*fs.appendFileSync("nuevoarchivo.txt", "data to append", "utf8")
 console.log(fs.appendFilesync)*/
 
-const copia = fs.copyFileSync("nuevoarchivo.txt", "nuevoarchivo2.txt")
-console.log(copia)
+/*const copia = fs.copyFileSync("nuevoarchivo.txt", "nuevoarchivo2.txt")
+console.log(copia)*/
+
+const unLinkSync = fs.unlinkSync("nuevoarchivo.txt")
