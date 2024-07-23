f64eeaf38a621b8186794d826eb5991ee6bc7a62
Pedro Corona <jorge.corona_86@hotmail.com>
2024-07-22 21:48:43 -0600

    segundo commit

---
+++ append-file.js
@@ -0,0 +1,7 @@
+const fs = require("node:fs")
+
+/*fs.appendFileSync("nuevoarchivo.txt", "data to append", "utf8")
+console.log(fs.appendFilesync)*/
+
+const copia = fs.copyFileSync("nuevoarchivo.txt", "nuevoarchivo2.txt")
+console.log(copia)
